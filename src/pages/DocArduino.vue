bf1f361e49adaadab9b43b2eb4c987dc1463d5ce
Roger Baracy <rogerbaracy.s@gmail.com>
2021-07-26 01:47:49 -0300

    Continuídade da documentação

--- src/pages/DocArduino.vue
+++ src/pages/DocArduino.vue
@@ -13,71 +13,25 @@
           <li><a href="#introducao">Introdução</a></li>
           <li><a href="#node">Criando um projeto Node</a></li>
           <li><a href="#johnny_five"> Instalando Johnny Five JS </a></li>
-          <li><a href="#dotnet"> Instalando Dotenv </a></li>
+          <li><a href="#dotenv"> Instalando Dotenv </a></li>
           <li><a href="#firebase"> Instalando Firebase </a></li>
         </ol>
       </q-card-section>
     </q-card>
-
-    <q-card class="q-mt-md">
-      <q-card-section
-        style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
-      >
-        <div class="text-h5 text-white" id="introducao">
-          Introdução
-        </div>
-      </q-card-section>
-      <q-card-section class="text-primary"> teste</q-card-section>
-    </q-card>
-
-    <q-card class="q-mt-md">
-      <q-card-section
-        style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
-      >
-        <div class="text-h5 text-white" id="node">
-          Criando um projeto Node
-        </div>
-      </q-card-section>
-      <q-card-section class="text-primary"> teste</q-card-section>
-    </q-card>
-
-    <q-card class="q-mt-md">
-      <q-card-section
-        style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
-      >
-        <div class="text-h5 text-white" id="johnny_five">
-          Instalando Johnny Five JS
-        </div>
-      </q-card-section>
-      <q-card-section class="text-primary"> teste</q-card-section>
-    </q-card>
-
-    <q-card class="q-mt-md">
-      <q-card-section
-        style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
-      >
-        <div class="text-h5 text-white" id="dotenv">
-          Instalando Dotenv
-        </div>
-      </q-card-section>
-      <q-card-section class="text-primary"> teste</q-card-section>
-    </q-card>
-
-    <q-card class="q-mt-md">
-      <q-card-section
-        style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
-      >
-        <div class="text-h5 text-white" id="firebase">
-          Instalando Firebase
-        </div>
-      </q-card-section>
-      <q-card-section class="text-primary"> teste</q-card-section>
-    </q-card>
+    <CardItem :propData="data"/>
   </q-page>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
-@Component
-export default class DocArduino extends Vue {}
+import CardItem from '../components/documentation/CardItems.vue';
+import data from './data/arduino';
+@Component({
+  components: {
+    CardItem
+  }
+})
+export default class DocArduino extends Vue {
+  private data = data;
+}
 </script>
---
+++ src/pages/data/arduino.ts
@@ -0,0 +1,35 @@
+const data = [
+  {
+    ancora: 'introducao',
+    title: 'Introdução',
+    content:
+      ['Em desenvolvimento ...']
+  },
+  {
+    ancora: 'node',
+    title: 'Criando um projeto Node',
+    content:
+      ['Em desenvolvimento ...']
+  },
+  {
+    ancora: 'johnny_five',
+    title: 'Instalando Johnny Five JS',
+    content:
+      ['Em desenvolvimento ...']
+  },
+  {
+    ancora: 'dotenv',
+    title: 'Instalando Dotenv',
+    content:
+      ['Em desenvolvimento ...']
+  },
+  {
+    ancora: 'firebase',
+    title: 'Instalando o Firebase',
+    content:
+      ['Em desenvolvimento ...']
+  },
+  
+  
+];
+export default data;
